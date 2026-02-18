bc9c821f1d5bf95b5ee72c46314bfad8d5c3a21a
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-02-18 14:36:25 +0530

    Replace SVG logo with PNG image in login page and add logo file to public directory

--- src/app/page/dashboard/dash-root/dash-root.ts
+++ src/app/page/dashboard/dash-root/dash-root.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dash-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './dash-root.html',
   styleUrl: './dash-root.css',
 })
